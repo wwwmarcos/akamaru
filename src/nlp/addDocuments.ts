2dafcfe3184eda392f032f77b5522ad24487bba1
marcos <eptaccio@gmail.com>
2020-06-03 22:00:39 -0300

    implement method

--- src/nlp/addDocuments.ts
+++ src/nlp/addDocuments.ts
@@ -5,7 +5,15 @@ const addDocuments = (options: {
   intents: IIntent[],
   language
 }) => {
-  console.log(options)
+  for (const intent of options.intents) {
+    for (const training of intent.training) {
+      options.nlp.addDocument(
+        options.language,
+        training,
+        intent.name
+      )
+    }
+  }
 }
 
 export {
