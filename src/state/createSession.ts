35049210cf3e861d445e28ef6c729f76e3389be6
marcos <eptaccio@gmail.com>
2020-06-05 15:20:56 -0300

    move function to new file

---
+++ src/state/createSession.ts
@@ -0,0 +1,12 @@
+import { UserSession } from '../interfaces/RikudoConfig'
+
+const createSession = (userId: string): UserSession => ({
+  userId,
+  currentStateName: 'START',
+  stack: [],
+  userData: {}
+})
+
+export {
+  createSession
+}
