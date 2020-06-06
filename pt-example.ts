c049491c54eeccbb0c4bc050937969bc268a7420
marcos <eptaccio@gmail.com>
2020-06-06 15:10:40 -0300

    add PT example

---
+++ pt-example.ts
@@ -0,0 +1,68 @@
+import { build, BotDefinition } from './index'
+
+const sessions = new Map()
+
+const saveSession = (userId, session) => {
+  sessions.set(userId, session)
+}
+
+const getSession = (userId) => {
+  return sessions.get(userId)
+}
+
+const botDefinition: BotDefinition = {
+  allIntents: [
+    {
+      name: 'BOLO',
+      training: ['bolo', 'quero um bolo', 'tem bolo ai?']
+    }
+  ],
+  language: 'pt',
+  states: [
+    {
+      name: 'START',
+      startTexts: ['Oi, como posso te ajudar?'],
+      actions: [{
+        onIntent: 'BOLO',
+        responses: ['Ok, tenho bolos aqui, qual vc quer?']
+      }],
+      unknownIntentAction: {
+        responses: ['Não te entendi, como posso ajudar?']
+      }
+    }
+  ],
+  resolvers: {
+    getSession,
+    saveSession
+  }
+}
+
+const start = async () => {
+  const app = await build(botDefinition)
+
+  const responses = [
+    await app.message({
+      text: 'oi',
+      userId: 'sample-id'
+    }),
+    await app.message({
+      text: 'quero um bolo',
+      userId: 'sample-id'
+    }),
+    await app.message({
+      text: 'patos não voam',
+      userId: 'sample-id'
+    })
+  ]
+
+  /**
+   * [
+   *   { response: 'Oi, Como posso te ajudar?' },
+   *   { response: 'Ok, tenho bolos aqui, qual vc quer?' },
+   *   { response: 'Não te entendi, como posso ajudar?' }
+   * ]
+   */
+  console.log(await Promise.all(responses))
+}
+
+start()
