2df6502cb4ffc660d272afe5e2626d35a9b9fd6f
marcos <eptaccio@gmail.com>
2020-06-06 11:19:11 -0300

    add usage example

---
+++ example.ts
@@ -0,0 +1,55 @@
+import { build, BotDefinition } from './index'
+
+const sessions = {}
+
+const saveSession = (userId, session) => {
+  sessions[userId] = session
+}
+
+const getSession = (userId) => {
+  return sessions[userId]
+}
+
+const allBotIntents = [{
+  name: 'GREETINGS',
+  training: ['hi', 'sup', 'hello']
+}]
+
+const startState = {
+  name: 'START',
+  startTexts: ['hi', 'welcome'],
+  actions: [{
+    onIntent: 'GREETINGS',
+    responses: ['hi, how i can help you?']
+  }],
+  unknownIntentAction: {
+    responses: ['wtf?']
+  }
+}
+
+const botDefinition: BotDefinition = {
+  allIntents: allBotIntents,
+  language: 'pt',
+  states: [
+    startState
+  ],
+  resolvers: {
+    getSession,
+    saveSession
+  }
+}
+
+const start = async () => {
+  const app = await build(botDefinition)
+
+  const response = await app.message({
+    text: 'hi',
+    userId: 'aidy'
+  })
+
+  console.log(
+    response
+  )
+}
+
+start();
