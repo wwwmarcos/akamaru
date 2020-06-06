40aeb5e81d903b55d9fbe6675127f5be3f45e247
marcos <eptaccio@gmail.com>
2020-06-06 11:18:54 -0300

    update build arguments

--- index.ts
+++ index.ts
@@ -1,27 +1,16 @@
 import { NlpManager } from 'node-nlp'
 import { INlpManager } from './src/interfaces/INlpManager'
-import { Intent } from './src/interfaces/Intent'
-import { Resolvers, RikudoConfig } from './src/interfaces/RikudoConfig'
+import { BotDefinition } from './src/interfaces/RikudoConfig'
 import { trainAndSave } from './src/nlp/trainAndSave'
 import { resolveMessage } from './src/state/resolveMessage'
 
-type IRikudoOptions = {
-  language: string
-  intents: Intent[]
-  botDefinition: RikudoConfig,
-  resolvers: Resolvers
-}
+const build = async (botDefinition: BotDefinition) => {
 
-const build = async ({
-  language = 'pt',
-  botDefinition,
-  resolvers
-}: IRikudoOptions) => {
+  const { allIntents, resolvers, language } = botDefinition
+
   const nlp: INlpManager = new NlpManager({
     languages: [language]
   })
-
-  const { allIntents } = botDefinition
 
   await trainAndSave({
     nlp,
@@ -38,5 +27,4 @@ const build = async ({
   }
 }
 
-export { build }
-
+export { build, BotDefinition }
